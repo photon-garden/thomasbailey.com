47fc62e88c425c1bb583b6db8ee756471b2df1dd
Thomas Bailey <thomas@noisemachines.io>
2020-07-29 18:01:31 -0400

    Write my first two ebombs.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,69 +1,61 @@
 module.exports = {
-	siteMetadata: {
-		title: `Thomas Bailey`,
-		name: `Thomas Bailey`,
-		siteUrl: `https://noisemachines.io`,
-		description: `This is my description that will be used in the meta tags and important for search results`,
-		hero: {
-			heading: `Welcome to Thomas. This is the place where I do the thing.`,
-			maxWidth: 652
-		},
-		social: [
-			{
-				name: `twitter`,
-				url: `https://twitter.com/narative`
-			},
-			{
-				name: `github`,
-				url: `https://github.com/narative`
-			},
-			{
-				name: `instagram`,
-				url: `https://instagram.com/narative.co`
-			},
-			{
-				name: `linkedin`,
-				url: `https://www.linkedin.com/company/narative/`
-			},
-			{
-				name: `dribbble`,
-				url: `https://dribbble.com/narativestudio`
-			}
-		]
-	},
-	plugins: [
-		{
-			resolve: '@narative/gatsby-theme-novela',
-			options: {
-				contentPosts: 'content/posts',
-				contentAuthors: 'content/authors',
-				basePath: '/',
-				authorsPage: true,
-				mailchimp: true,
-				sources: {
-					local: true
-					// contentful: true,
-				}
-			}
-		},
-		{
-			resolve: 'gatsby-plugin-mailchimp',
-			options: {
-				endpoint:
-					'https://thomasbailey.us10.list-manage.com/subscribe/post?u=ba4db42a0195565ed932faaf5&amp;id=4d854d6f81' // add your MC list endpoint here; see plugin repo for instructions
-			}
-		},
-		{
-			resolve: `gatsby-plugin-manifest`,
-			options: {
-				name: `Novela by Narative`,
-				short_name: `Novela`,
-				start_url: `/`,
-				background_color: `#fff`,
-				theme_color: `#fff`,
-				display: `standalone`,
-				icon: `src/assets/favicon.png`
-			}
-		}
-	]
+  siteMetadata: {
+    title: `Thomas Bailey`,
+    name: `Thomas Bailey`,
+    siteUrl: `https://thomasbailey.com`,
+    description: `Love Svelte but get stuck sometimes? I'm here to help.`,
+    hero: {
+      heading: `Love Svelte but get stuck sometimes? I'm here to help.`,
+      maxWidth: 652
+    },
+    social: [
+      {
+        name: `twitter`,
+        url: `https://twitter.com/noise_machines`
+      },
+      {
+        name: `github`,
+        url: `https://github.com/noise-machines`
+      },
+      {
+        name: `linkedin`,
+        url: `https://www.linkedin.com/company/datajoy/`
+      }
+    ]
+  },
+  plugins: [
+    {
+      resolve: '@narative/gatsby-theme-novela',
+      options: {
+        contentPosts: 'content/posts',
+        contentAuthors: 'content/authors',
+        basePath: '/',
+        authorsPage: true,
+        mailchimp: true,
+        sources: {
+          local: true
+          // contentful: true,
+        }
+      }
+    },
+    {
+      resolve: 'gatsby-plugin-mailchimp',
+      options: {
+        endpoint:
+          'https://thomasbailey.us10.list-manage.com/subscribe/post?u=ba4db42a0195565ed932faaf5&amp;id=4d854d6f81' // add your MC list endpoint here; see plugin repo for instructions
+      }
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Thomas Bailey`,
+        short_name: `Thomas Bailey`,
+        start_url: `/`,
+        background_color: `#fff`,
+        theme_color: `#fff`,
+        display: `standalone`,
+        icon: `src/assets/favicon.png`
+      }
+    }
+  ]
 }
--- src/@narative/gatsby-theme-novela/components/Logo/index.js
+++ src/@narative/gatsby-theme-novela/components/Logo/index.js
@@ -9,61 +9,61 @@ import React from 'react'
  */
 
 function Svg ({ fill }) {
-	const strokeWidth = 10
+  const strokeWidth = 10
 
-	return (
-		<svg
-			width='30'
-			height='26'
-			viewBox='0 0 253 219'
-			fill='none'
-			xmlns='http://www.w3.org/2000/svg'
-		>
-			<path
-				d='M41.0625 171.438L66.6875 171.562L137.219 48.8438L238.216 217.151L250.767 194.258L137.562 2.25L41.0625 171.438Z'
-				stroke={fill}
-				strokeWidth={strokeWidth}
-				strokeLinecap='round'
-				strokeLinejoin='round'
-			/>
-			<path
-				d='M1.625 195.062L13.5 217.438L238.305 217.283L137.232 48.8593L124.549 70.9343L197.734 194.927L1.625 195.062Z'
-				stroke={fill}
-				strokeWidth={strokeWidth}
-				strokeLinecap='round'
-				strokeLinejoin='round'
-			/>
-			<path
-				d='M110.056 1.9906L1.63879 195.025L197.764 194.968L183.881 171.512L40.949 171.532L137.674 2.25381L110.056 1.9906Z'
-				stroke={fill}
-				strokeWidth={strokeWidth}
-				strokeLinecap='round'
-				strokeLinejoin='round'
-			/>
-		</svg>
-	)
+  return (
+    <svg
+      width='30'
+      height='26'
+      viewBox='0 0 253 219'
+      fill='none'
+      xmlns='http://www.w3.org/2000/svg'
+    >
+      <path
+        d='M41.0625 171.438L66.6875 171.562L137.219 48.8438L238.216 217.151L250.767 194.258L137.562 2.25L41.0625 171.438Z'
+        stroke={fill}
+        strokeWidth={strokeWidth}
+        strokeLinecap='round'
+        strokeLinejoin='round'
+      />
+      <path
+        d='M1.625 195.062L13.5 217.438L238.305 217.283L137.232 48.8593L124.549 70.9343L197.734 194.927L1.625 195.062Z'
+        stroke={fill}
+        strokeWidth={strokeWidth}
+        strokeLinecap='round'
+        strokeLinejoin='round'
+      />
+      <path
+        d='M110.056 1.9906L1.63879 195.025L197.764 194.968L183.881 171.512L40.949 171.532L137.674 2.25381L110.056 1.9906Z'
+        stroke={fill}
+        strokeWidth={strokeWidth}
+        strokeLinecap='round'
+        strokeLinejoin='round'
+      />
+    </svg>
+  )
 }
 
 export default function Logo ({ fill }) {
-	const logoStyle = {
-		fontSize: '20px'
-	}
-	const thomasStyle = {
-		color: fill,
-		paddingLeft: '10px'
-	}
-	const baileyStyle = {
-		color: 'hsl(208, 5%, 50%)',
-		fontWeight: 400
-	}
+  const logoStyle = {
+    fontSize: '20px'
+  }
+  const thomasStyle = {
+    color: fill,
+    paddingLeft: '10px'
+  }
+  const baileyStyle = {
+    color: 'hsl(208, 5%, 50%)',
+    fontWeight: 400
+  }
 
-	return (
-		<>
-			<Svg fill={fill} />
-			<h1 style={logoStyle}>
-				<span style={thomasStyle}>THOMAS</span>{' '}
-				<span style={baileyStyle}>BAILEY</span>
-			</h1>
-		</>
-	)
+  return (
+    <>
+      <Svg fill={fill} />
+      <h1 style={logoStyle}>
+        <span style={thomasStyle}>THOMAS</span>{' '}
+        <span style={baileyStyle}>BAILEY</span>
+      </h1>
+    </>
+  )
 }
